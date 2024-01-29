a2cac3b42322408581a22c2388bce73cd611e12b
janphan <janphan061@gmail.com>
2024-01-29 12:30:02 +0200

    Assignment 2 Handling lists

---
+++ Handling_Lists/handling_lists/src/main/java/handling_lists/web/StudentController.java
@@ -0,0 +1,25 @@
+package handling_lists.web;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import handling_lists.domain.Student;
+
+@Controller
+public class StudentController {
+    @GetMapping("/hello")
+    public String handlingStudentList(Model model) {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Hong", "Phan"));
+        students.add(new Student("John", "Smith"));
+        students.add(new Student("Joe", "Doe"));
+
+        model.addAttribute("studentlistInHTML", students.toArray());
+        return "studentlist";
+    }
+
+}
