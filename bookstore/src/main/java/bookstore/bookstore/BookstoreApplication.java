379531bda44ffd7ad822f2b37e007a7694f1267e
janphan <janphan061@gmail.com>
2024-02-05 16:46:42 +0200

    update bookstore with add and delete

--- bookstore/src/main/java/bookstore/bookstore/BookstoreApplication.java
+++ bookstore/src/main/java/bookstore/bookstore/BookstoreApplication.java
@@ -1,13 +1,34 @@
 package bookstore.bookstore;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import bookstore.bookstore.domain.Book;
+import bookstore.bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner demo(BookRepository repository) {
+		return (args) -> {
+			repository.save(new Book("The Night Circus", "Erin Morgenstern", 2011, "978-0385534635", 15));
+			repository.save(new Book("The Martia", "Andy Weir", 2014, "978-0553418026", 14));
+			repository.save(new Book("Normal People", "Sally Rooney", 2018, "978-1984822178", 14));
+
+			for (Book book : repository.findAll()) {
+				log.info(book.toString());
+			}
+		};
+	}
 }
--- bookstore/src/main/java/bookstore/bookstore/domain/Book.java
+++ bookstore/src/main/java/bookstore/bookstore/domain/Book.java
@@ -1,6 +1,15 @@
 package bookstore.bookstore.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String title;
     private String author;
     private int year;
@@ -11,6 +20,7 @@ public class Book {
     }
 
     public Book(String title, String author, int year, String isbn, double price) {
+        super();
         this.title = title;
         this.author = author;
         this.year = year;
@@ -18,43 +28,56 @@ public class Book {
         this.price = price;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public void setTitle(int year) {
-        this.year = year;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
+    public Book(Long id) {
+        this.id = id;
     }
 
     public String getTitle() {
         return title;
     }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     public String getAuthor() {
         return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
     }
 
     public int getYear() {
         return year;
     }
 
+    public void setYear(int year) {
+        this.year = year;
+    }
+
     public String getIsbn() {
         return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
     }
 
     public double getPrice() {
         return price;
     }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
 }
