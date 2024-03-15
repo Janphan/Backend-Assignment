2ee8e96207d737e11518f4577679dc3ec80c304b
janphan <janphan061@gmail.com>
2024-03-15 17:51:34 +0200

    delete unused files

--- bookstoreSpringDeploy/src/main/java/bookstore/bookstore/domain/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package bookstore.bookstore.domain;
-
-import org.springframework.data.repository.CrudRepository;
-import java.util.List;
-
-public interface BookRepository extends CrudRepository<Book, Long> {
-    List<Book> findByAuthor(String author);
-}
